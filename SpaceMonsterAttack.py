0ba087f49930795ca6744ec8e6141de65bfde435
Mihail Georgescu <geo.mihail96@gmail.com>
2019-06-14 02:51:02 +0300

    Cleaner code

--- SpaceMonsterAttack.py
+++ SpaceMonsterAttack.py
@@ -3,7 +3,7 @@ import random
 
 pygame.init()
 
-window = pygame.display.set_mode((800,600))
+window = pygame.display.set_mode((800, 600))
 bck = pygame.image.load("media/background.jpeg")
 paleta = pygame.image.load("media/paleta.png")
 proiectil = pygame.image.load("media/proiectil.png")
@@ -11,7 +11,7 @@ monstru = pygame.image.load("media/monstru.png")
 explozie = pygame.image.load("media/boom.png")
 go = pygame.image.load("media/gameover.png")
 muzica = pygame.mixer.music.load("media/sma-music.mp3")
-font = pygame.font.SysFont('comicsans',30,True,False)
+font = pygame.font.SysFont('comicsans', 30, True, False)
 
 pygame.display.set_caption("Space Monster Attack")
 pygame.mixer.music.play(-1)
@@ -20,14 +20,16 @@ x = 365
 y = 570
 speed = 8
 
+
 class Projectile(object):
     def __init__(self, xcor, ycor, exists):
-        self.xcor=xcor
-        self.ycor=ycor
-        self.exists=exists
+        self.xcor = xcor
+        self.ycor = ycor
+        self.exists = exists
 
-class monster(object):
-    def __init__ (self, xm, ym, alive):
+
+class Monster(object):
+    def __init__(self, xm, ym, alive):
         self.xm = xm
         self.ym = ym
         self.alive = alive
@@ -50,10 +52,10 @@ while run:
     keys = pygame.key.get_pressed()
 
     if (keys[pygame.K_LEFT]) and x > 5:
-        x-=speed
+        x -= speed
 
     if (keys[pygame.K_RIGHT]) and x < 724:
-        x+=speed
+        x += speed
 
     if difficulty <= 7:
         difficulty += 0.0002
@@ -64,42 +66,47 @@ while run:
         p1 = Projectile(x+28, y-45, True)
         shoot = True
 
-    window.blit(bck,(0,0))
-    window.blit(paleta,(x,y))
+    window.blit(bck, (0, 0))
+    window.blit(paleta, (x, y))
 
-    text = font.render('Score: ' + str(score), 1, (250,250,250))
+    text = font.render('Score: ' + str(score), 1, (250, 250, 250))
     window.blit(text,(20, 20))
+
     if shoot:
-         if p1.exists == True:
-             window.blit(proiectil, (p1.xcor, p1.ycor))
-             p1.ycor -= 15
-             if p1.ycor < 0:
-                 p1.exists = False;
-         else:
-             shoot = False
+        if p1.exists:
+            window.blit(proiectil, (p1.xcor, p1.ycor))
+            p1.ycor -= 15
+            if p1.ycor < 0:
+                p1.exists = False
+        else:
+            shoot = False
 
     if clock * difficulty > 50:
          clock = 0
          rand = random.randint(0, 6)
-         enemies.append(monster(rand * 100 + 150, 0, True))
+         enemies.append(Monster(rand * 100 + 150, 0, True))
 
     for e in enemies:
-        if e.alive == True:
-            window.blit(monstru,(e.xm,e.ym))
+
+        if e.alive:
+            window.blit(monstru, (e.xm, e.ym))
             e.ym += 3 * difficulty
+
         if shoot:
-            if p1.ycor < e.ym + 15 and p1.xcor < e.xm + 40 and p1.xcor > e.xm - 20 and p1.exists == True and e.alive == True:
-                if e.alive == True:
-                    window.blit(explozie,(e.xm,e.ym))
+            if p1.ycor < e.ym + 15 and p1.xcor < e.xm + 40 and p1.xcor > e.xm - 20 and p1.exists and e.alive:
+                window.blit(explozie, (e.xm, e.ym))
                 e.alive = False
                 p1.exists = False
                 score += 100
+
         if e.ym > 570:
-            window.blit(go,(250, 230))
+            window.blit(go, (250, 230))
             dead = True
+
     pygame.display.update()
-    if dead == True:
+
+    if dead:
         pygame.time.delay(3000)
         run = False
 
-print ("Your score was: ", score)
+print("Your score was: ", score)
